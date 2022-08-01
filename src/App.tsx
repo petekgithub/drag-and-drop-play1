05014eea8a463e63ec259473a621e91ba2a55204
petekgithub <petekshamitbeyli@gmail.com>
2022-08-01 21:38:30 +0300

    typescript-react-draganddrop

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const listItems = [
+  {
+    id: "1",
+    name: "Workout"
+
+  },
+  {
+    id: "2",
+    name: "Eat Popcorn"
+
+  },
+  {
+    id: "3",
+    name: "Watch: Big Lebowski"
+
+  }, {
+    id: "4",
+    name: "Catch the Bee"
+
+  }
+]
+ 
+
+
 function App() {
+  const [todo, setTodo] = useState(listItems);
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
